421be067c2143135e421f5c7361073d66f921f1e
bhanuprakash212 <52093625+bhanuprakash212@users.noreply.github.com>
2019-06-29 15:31:25 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,16 @@
+# Drawing a Square
+import turtle as tt
+a1=tt.Turtle()
+a1.forward(150)
+a1.right(90)
+
+a1.forward(150)
+a1.right(90)
+
+a1.forward(150)
+a1.right(90)
+
+a1.forward(150)
+a1.right(90)
+
+tt.done()
